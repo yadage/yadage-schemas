7991f97bb3113f680dd1d9d59a62a192307e5d01
lukasheinrich <lukas.heinrich@gmail.com>
2016-07-14 04:16:48 +0200

    bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     author = 'Lukas Heinrich',
     author_email = 'lukas.heinrich@gmail.com',
     name = 'cap-schemas',
-    version = '0.0.4',
+    version = '0.1.0',
     description = 'schemas for analysis preservation',
     include_package_data = True,
     packages = find_packages(),

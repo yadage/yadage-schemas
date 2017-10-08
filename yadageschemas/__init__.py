22408d531bfeb9131cd1fa6258662a1fb7d222aa
lukasheinrich <lukas.heinrich@gmail.com>
2017-10-08 21:14:01 +0200

    guard against instance being non-dict when setting defaults

--- yadageschemas/__init__.py
+++ yadageschemas/__init__.py
@@ -53,7 +53,7 @@ def extend_with_default(validator_class):
         if errors_found: return
 
         for prop, subschema in properties.items():
-            if "default" in subschema:
+            if "default" in subschema and type(instance) == dict:
                 instance.setdefault(prop, subschema["default"])
 
     return validators.extend(

22b078100b341a233f5f271f3f3d1f14a8ff4438
lukasheinrich <lukas.heinrich@gmail.com>
2017-10-08 17:05:16 +0200

    * fix tests

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires = [
         'jsonref',
         'pyyaml',
-        'requests[security]',
+        'requests[security]>=2.9',
         'jsonschema',
         'click'
     ],

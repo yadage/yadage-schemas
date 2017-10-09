107adfd5b462cf90b049b72d1353175554733bf3
Lukas <lukas.heinrich@gmail.com>
2017-10-09 11:49:55 +0200

    unwrap type must be bool (#18)
    * unwrap type must be bool
    
    * add tests
    
    * revert default true.. need to think a bit

--- tests/test_load.py
+++ tests/test_load.py
@@ -4,3 +4,7 @@ import json
 def test_load_jsondump():
     data = yadageschemas.load('workflow.yml','tests/testspecs/local-helloworld-jsonref', 'yadage/workflow-schema')
     json.dumps(data)
+
+def test_load_default_unwrap():
+    data = yadageschemas.load('workflow.yml','tests/testspecs/mapreduce', 'yadage/workflow-schema')
+    json.dumps(data)

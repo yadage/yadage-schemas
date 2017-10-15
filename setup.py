ec9777403eb973768e097fe16572ab633b88fd3d
lukasheinrich <lukas.heinrich@gmail.com>
2017-10-15 00:50:09 -0500

    Bump version: 0.7.11 → 0.7.12

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     author = 'Lukas Heinrich',
     author_email = 'lukas.heinrich@gmail.com',
     name = 'yadage-schemas',
-    version = '0.7.11',
+    version = '0.7.12',
     description = 'schemas for yadage and packtivity',
     include_package_data = True,
     packages = find_packages(),

f16484ec704b523f442c5192d34bb51ca1596584
Matthew Feickert <matthew.feickert@cern.ch>
2021-12-14 11:49:03 -0600

    ci: Add GitHub Actions based CI (#37)
    * Remove Travis CI
    * Add GitHub Actions based CI to:
    * test against Python 2.7, and Python 3.6 to Python 3.10
    * lint using pyflakes
    * test package builds using build
    * Add python_requires to setup.py to require Python 2.7 or Python 3.6+
    * Update MANIFEST.in for a valid build
    * Add PyPI metadata classifiers to setup.py
    * Add MIT LICENSE

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 [![DOI](https://zenodo.org/badge/54061494.svg)](https://zenodo.org/badge/latestdoi/54061494)
 [![PyPI version](https://badge.fury.io/py/yadage-schemas.svg)](https://badge.fury.io/py/yadage-schemas)
-[![Build Status](https://travis-ci.org/diana-hep/yadage-schemas.svg?branch=master)](https://travis-ci.org/diana-hep/yadage-schemas)
+
+[![CI](https://github.com/yadage/yadage-schemas/actions/workflows/ci.yml/badge.svg)](https://github.com/yadage/yadage-schemas/actions/workflows/ci.yml?query=branch%3Amaster)
 
 This package holds JSON schema definitions for preserving individual processing tasks of scientific workflows (referred to "packtivities" since they including information where to find their respective prepackage sofware environments) as well as schemas to define declaratively workflows that orchestrate multiple of these steps using directed acyclic graphs (DAGs)
 
 Workflows defined this way can be read and executed by these packages:
 
-Packtivity: https://github.com/diana-hep/packtivity
-Yadage: https://github.com/lukasheinrich/yadage 
+* Packtivity: https://github.com/yadage/packtivity
+* Yadage: https://github.com/yadage/yadage
 
--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     description = 'schemas for yadage and packtivity',
     include_package_data = True,
     packages = find_packages(),
+    # Support Python 3.6+ but keep legacy support for Python 2.7 until 2022
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     install_requires = [
         'jsonref',
         'pyyaml',
@@ -28,5 +30,22 @@ setup(
       'console_scripts': [
           'yadage-validate=yadageschemas.validatecli:main',
       ],
-    }
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Operating System :: OS Independent",
+    ]
 )

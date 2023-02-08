a51828b5f781567ecd9fe3ad188e73f8617e33a6
Matthew Feickert <matthew.feickert@cern.ch>
2023-02-08 16:10:17 -0600

    feat: Update to draft 06 of json schema

--- yadageschemas/validator.py
+++ yadageschemas/validator.py
@@ -3,11 +3,11 @@ from .utils import schema_and_refresolver
 
 from .dialects import raw_with_defaults
 assert raw_with_defaults
-from jsonschema import Draft4Validator
+from jsonschema import Draft6Validator
 
 def validator(schema_name,schemadir):
     schema, resolver = schema_and_refresolver(schema_name,schemadir)
-    return Draft4Validator(schema, resolver = resolver)
+    return Draft6Validator(schema, resolver = resolver)
 
 def validate_spec(data, validopts):
     return validator(**validopts).validate(data)

a9c96babaae349ed959509bf4e409693288a86ba
Matthew Feickert <matthew.feickert@cern.ch>
2024-06-17 11:51:06 -0500

    revert import order

--- yadageschemas/__init__.py
+++ yadageschemas/__init__.py
@@ -7,11 +7,13 @@ except ImportError:
     # https://docs.python.org/3/library/importlib.resources.html#importlib.resources.files
     from importlib_resources import files
 
+# FIXME: Import order must be before yadageschemas.utils to avoid circular import
+schemadir = files("yadageschemas")
+
 from . import dialects
 from .utils import WithJsonRefEncoder
 from .validator import validate_spec
 
-schemadir = files("yadageschemas")
 
 def load(spec, specopts, validate = True, validopts = None, dialect = 'raw_with_defaults'):
     data = dialects.handlers[dialect](spec, specopts)
